3abeb857cb4de79c88f227bea460768388b09164
DVD03 <dilumvishvajith18@gmail.com>
2026-09-17 16:51:55 +0530

    Include server files in Vercel bundle and add initialization wrapper

--- api/index.js
+++ api/index.js
@@ -1,3 +1,18 @@
-﻿const app = require("../server/src/app");
+let app;
+try {
+  app = require("../server/src/app");
+} catch (err) {
+  console.error("Critical error loading Express app:", err);
+  const express = require("express");
+  app = express();
+  app.all("*", (_req, res) => {
+    res.status(500).json({
+      error: "Critical Server Initialization Failure",
+      message: err.message,
+      stack: err.stack
+    });
+  });
+}
 
 module.exports = app;
+
